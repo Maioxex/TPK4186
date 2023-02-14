c12e689b5969babc17a00a7f371e6a2ad5c7f65a
Maioxex <48554030+Maioxex@users.noreply.github.com>
2023-02-14 17:54:41 +0100

    ge

---
+++ Øving 1/Containers.py
@@ -0,0 +1,6 @@
+class container:
+    def __init__(length, id, load =0):
+        self.length = length
+        self.id = id
+        self.load = load
+        
---
+++ Øving 1/Ships.py
@@ -0,0 +1,8 @@
+class ships:
+    def __init__(self, id, length, width, height):
+        self.length = length
+        self.width = width
+        self.height = height
+        self.id = id
+        
+        
