e0b5b1f56058ab92b3e8f87c5b6cd9aca0dd193d
Maioxex <48554030+Maioxex@users.noreply.github.com>
2023-02-14 18:03:58 +0100

    Update Containers.py

--- Øving 1/Containers.py
+++ Øving 1/Containers.py
@@ -1,6 +1,36 @@
 class container:
+    self.length = 0
+    
     def __init__(length, id, load =0):
         self.length = length
         self.id = id
         self.load = load
+        if length == 20:
+            self.selvvekt = 2
+        else:
+            self.selvvekt = 4
+    
+    #make the getter and setter functions for container
+    def getLength(self):
+        return self.length
+    
+    def setLength(self, length):
+        self.length = length
+    
+    def getId(self):
+        return self.id
+    
+    def setId(self, id):
+        self.id = id
+    
+    def getLoad(self):
+        return self.load
+    
+    def setLoad(self, load):
+        self.load = load
         
+    def getTotalWeight(self):
+
+        return self.load + selv
+    
+    
