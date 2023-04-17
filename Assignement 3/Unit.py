c2d12614a473553e9ee661b4539460a98be286c0
Maioxex <48554030+Maioxex@users.noreply.github.com>
2023-04-17 17:28:08 +0200

    Update Unit.py

--- Assignement 3/Unit.py
+++ Assignement 3/Unit.py
@@ -10,6 +10,8 @@ class units:
         self.productiontimes = [0.5,3.5,1.2,3,0.8,0.5,1,1.9,0.3]
         self.currenttask = -1
         self.batch = -1
+        self.state = -1  
+        self.possiblestates = ["idle", "loading", "processing", "unloading"]
     
     def getTasks(self):
         return self.tasks
@@ -49,14 +51,49 @@ class units:
         self.time = 1
         pl.choosebatch(self)
         self.batch = batch
+        self.state = "loading"
 
     def unloadTask(self):
         if self.time != 0:
-            raise ValueError("Unit is busy")
+            raise ValueError(f"Unit is busy being {self.state}")
         self.currenttask = -1
         self.time = 1
+        tempbatch = self.batch
+        self.batch = -1
+        self.state = "unloading"
+        return tempbatch
     
-    def starttask(self):
+    def startTask(self):
         if self.time != 0:
-            raise ValueError("Unit is busy")
+            raise ValueError(f"Unit is busy being {self.state}")
         self.time = self.productiontimes[self.currenttask]*self.batch.getSize()
+    
+    def getState(self):
+        return self.state
+    def setState(self, state):
+        self.state = state
+    def getBatch(self):
+        return self.batch
+    def setBatch(self, batch):
+        self.batch = batch
+    def checkState(self):
+        if self.state == "loading":
+            if self.time == 0:
+                self.state = "processing"
+                self.startTask()
+        elif self.state == "processing":
+            if self.time == 0:
+                self.state = "unloading"
+                self.unloadTask()
+        elif self.state == "unloading":
+            if self.time == 0:
+                self.state = "idle"
+        elif self.state == "idle":
+            pl.choosebatch(self)
+        else:
+            raise ValueError("State not possible")
+    def isIdle(self):
+        if self.state == "idle":
+            return True
+        else:
+            return False
