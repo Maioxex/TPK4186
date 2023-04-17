dc8599a188a27536745c3a526310f835b20ab3db
Maioxex <48554030+Maioxex@users.noreply.github.com>
2023-04-17 17:15:40 +0200

    g

--- Assignement 3/Unit.py
+++ Assignement 3/Unit.py
@@ -1,9 +1,15 @@
+from Batch import batch
+from Buffers import buffer
+from Productionline import productionline as pl
+
+
 class units:
     def __init__(self, tasks):
         self.time = 0
         self.tasks = tasks
         self.productiontimes = [0.5,3.5,1.2,3,0.8,0.5,1,1.9,0.3]
         self.currenttask = -1
+        self.batch = -1
     
     def getTasks(self):
         return self.tasks
@@ -28,3 +34,29 @@ class units:
         
     def setTime(self, time):
         self.time = time
+    
+    def TimeIncrement(self):
+        self.time += 1
+        
+    def loadTask(self, task):
+        if self.time != 0:
+            raise ValueError("Unit is busy")
+        if task not in self.tasks:
+            raise ValueError("Task not possible")
+        else: 
+            self.currenttask = task
+            self.time = self.productiontimes[task]
+        self.time = 1
+        pl.choosebatch(self)
+        self.batch = batch
+
+    def unloadTask(self):
+        if self.time != 0:
+            raise ValueError("Unit is busy")
+        self.currenttask = -1
+        self.time = 1
+    
+    def starttask(self):
+        if self.time != 0:
+            raise ValueError("Unit is busy")
+        self.time = self.productiontimes[self.currenttask]*self.batch.getSize()
