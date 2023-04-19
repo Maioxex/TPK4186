a79cd53cb16f73a0a2f51a02ab9a0ffe2d5e5f94
Maioxex <48554030+Maioxex@users.noreply.github.com>
2023-04-19 21:09:35 +0200

    idfk

--- Assignement 3/Productionline.py
+++ Assignement 3/Productionline.py
@@ -15,7 +15,9 @@ class productionline:
             self.units.append(units(tasks[i]), i)
         for i in range(10):
             self.buffers.append(buffers(i))
-        
+    
+    def loadBatchToInputBuffer(self, batch):
+        self.buffers[0].addBatch(batch)
         
     def findUnitWithTask(self, task):
         for unit in self.units:
@@ -68,9 +70,12 @@ class productionline:
             raise ValueError("Unit is busy")
         elif task not in unit.getTasks():
             raise ValueError("Task not possible")
+        elif batch.getTask() != task:
+            raise ValueError("Batch not possible")
         unit.setTask(task)
         unit.setTime(1)
         unit.setBatch(batch)
+        batch.incrementCurrentTask()
         unit.setState("loading")
 
     def unloadTask(self, unit):
@@ -79,9 +84,17 @@ class productionline:
         unit.setTask(-1)
         unit.setTime(1)
         tempbatch = unit.getBatch()
-        unit.setBatch(-1)
         unit.setState("unloading")
         return tempbatch
+    
+    def unloadUnit(self, unit):
+        if unit.getTime() != 0:
+            raise ValueError(f"Unit is busy being {unit.state}")
+        unit.setTask(-1)
+        batch = unit.getBatch()
+        self.getBufferWithTask(batch.getTask()).addBatch(batch)
+        unit.setBatch(-1)
+        unit.setState("idle")
     
     def startTask(self, unit):
         if self.time != 0:
@@ -89,64 +102,3 @@ class productionline:
         unit.setTime(unit.productiontimes[unit.getTask()]*unit.getBatch().getSize())
         unit.setState("processing")
     
-    def loadUnitWithBatch(self, batch, unit):
-        unit.loadTask(batch.getTask(), batch)
-    
-    
-    
-    
-    
-    
-    
-    def chooseBatchForUnit(self, unit, delta = 0.1):
-        if unit.isBusy():
-            raise ValueError("Unit is busy")
-        else:
-            unitID = unit.getID()
-            unitbuffers = []
-            for buffernr in self.tasks[unitID]:
-                unitbuffers.append(self.getBufferWithTask(buffernr))
-            bufferlist = []
-            for buffer in unitbuffers:
-                if not buffer.isBusy():
-                    bufferlist.append(buffer)
-            if len(bufferlist) == 0:
-                return "No buffer loaded"
-            else:
-                bufferlist.sort(key  = lambda x: x.getLoadRatio())
-                if (self.buffers[0].getLoadRatio(self.numberofwafers, True) >= 1-delta or self.buffers[0].getLoadRatio(self.numberofwafers, True) >= 1-delta) and self.buffers[0] in bufferlist:
-                    buffer = self.buffers[0]
-                    batch = self.chooseBatchForBuffer(buffer, smallest = True)
-                    unit.loadTask(batch.getTask(), batch)
-                    return batch
-                else:
-                    buffer = bufferlist[0]
-                    batch = self.chooseBatchForBuffer(buffer, smallest = False)
-                    unit.loadTask(batch.getTask(), batch)
-                return batch
-    
-    def chooseBatchForBuffer(self, buffer, smallest = False):
-        if buffer.isBusy():
-            raise ValueError("Buffer is busy")
-        else:
-            proofedlist = []
-            for batch in buffer.batches:
-                if self.canUnloadUnitWitchBatch(batch):
-                    proofedlist.append(batch)
-            if len(proofedlist) == 0:
-                return "No batch can be unloaded"
-            else:
-                if smallest:
-                    proofedlist.sort(key = lambda x: x.getSize())
-                    return proofedlist[0]
-                else:
-                    proofedlist.sort(key = lambda x: x.getSize(), reverse = True)
-                    return proofedlist[0]    
-    
-    def canUnloadUnitWitchBatch(self, batch):
-        nextBuffer = self.buffer[batch.getTask()+1]
-        unit = self.findUnitWithBuffer(nextBuffer)
-        if nextBuffer.getLoad()+batch.getLoad() <= nextBuffer.getLimit():
-            return True
-        else:
-            return False
---
+++ Assignement 3/SImulator.py
@@ -0,0 +1,71 @@
+import Productionline as pl
+
+
+class simulator:
+    def __init__(self, numberofwafers):
+        self.pl = pl.productionline(numberofwafers)
+    
+    def getPL(self):
+        return self.pl
+       
+    def set(self, pl):
+        self.pl = pl
+    
+    def getTime(self):
+        return self.pl.time
+    
+    def loadBatchToInputBuffer(self, batch):
+        self.pl.loadBatchToInputBuffer(batch)
+        
+    def chooseBatchForUnit(self, unit, delta = 0.1):
+        if unit.isBusy():
+            raise ValueError("Unit is busy")
+        else:
+            unitID = unit.getID()
+            unitbuffers = []
+            for buffernr in self.pl.tasks[unitID]:
+                unitbuffers.append(self.pl.getBufferWithTask(buffernr))
+            bufferlist = []
+            for buffer in unitbuffers:
+                if not buffer.isBusy():
+                    bufferlist.append(buffer)
+            if len(bufferlist) == 0:
+                return "No buffer loaded"
+            else:
+                bufferlist.sort(key  = lambda x: x.getLoadRatio())
+                if (self.pl.buffers[0].getLoadRatio(self.pl.numberofwafers, True) >= 1-delta or self.pl.buffers[0].getLoadRatio(self.pl.numberofwafers, True) >= 1-delta) and self.pl.buffers[0] in bufferlist:
+                    buffer = self.pl.buffers[0]
+                    batch = self.pl.chooseBatchForBuffer(buffer, smallest = True)
+                    unit.loadTask(batch.getTask(), batch)
+                    return batch
+                else:
+                    buffer = bufferlist[0]
+                    batch = self.pl.chooseBatchForBuffer(buffer, smallest = False)
+                    unit.loadTask(batch.getTask(), batch)
+                return batch
+    
+    def chooseBatchForBuffer(self, buffer, smallest = False):
+        if buffer.isBusy():
+            raise ValueError("Buffer is busy")
+        else:
+            proofedlist = []
+            for batch in buffer.batches:
+                if self.pl.canUnloadUnitWitchBatch(batch):
+                    proofedlist.append(batch)
+            if len(proofedlist) == 0:
+                return "No batch can be unloaded"
+            else:
+                if smallest:
+                    proofedlist.sort(key = lambda x: x.getSize())
+                    return proofedlist[0]
+                else:
+                    proofedlist.sort(key = lambda x: x.getSize(), reverse = True)
+                    return proofedlist[0]    
+    
+    def canUnloadUnitWitchBatch(self, batch):
+        nextBuffer = self.pl.buffer[batch.getTask()+1]
+        unit = self.pl.findUnitWithBuffer(nextBuffer)
+        if nextBuffer.getLoad()+batch.getLoad() <= nextBuffer.getLimit():
+            return True
+        else:
+            return False
