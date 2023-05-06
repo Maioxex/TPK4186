16ccf4cf10abea3668bbb0ce9643fc9fdbc34cba
Maioxex <48554030+Maioxex@users.noreply.github.com>
2023-05-06 16:16:37 +0200

    formatting

--- Assignment 4/Node.py
+++ Assignment 4/Node.py
@@ -1,7 +1,8 @@
 import numpy as np
 
+
 class node:
-    def __init__(self, name, time, predecessors = None, successors = None, finished = False, description = None):
+    def __init__(self, name, time, predecessors=None, successors=None, finished=False, description=None):
         self.name = name
         self.finished = finished
         self.predecessors = predecessors
@@ -13,103 +14,133 @@ class node:
         self.lateFinish = False
         if self.time != None:
             self.duration = time[1]
-        else: 
+        else:
             self.duration = None
         self.description = description
         self.critical = False
-    
+
     def isCritical(self):
         return self.critical
+
     def setCritical(self, critical):
         self.critical = critical
+
     def getName(self):
         return self.name
+
     def setName(self, name):
         self.name = name
+
     def getPredecessor(self):
         return self.predecessors
+
     def setPredecessor(self, predecessors):
-        self.predecessors = predecessors    
+        self.predecessors = predecessors
+
     def getSuccessor(self):
         return self.successors
+
     def setSuccessor(self, successors):
         self.successors = successors
+
     def setTime(self, time):
         self.time = time
+
     def getTime(self):
         return self.time
+
     def setMinTime(self, minTime):
         self.time[0] = minTime
+
     def getMinTime(self):
         return self.time[0]
+
     def getMaxTime(self):
         return self.time[2]
+
     def setMaxTime(self, endtime):
         self.time[2] = endtime
+
     def getExpected(self):
         return self.time[1]
+
     def setExpected(self, expected):
         self.time[1] = expected
+
     def getFinished(self):
         return self.finished
+
     def setFinished(self, finished):
         self.finished = finished
+
     def getEarlyStart(self):
         return self.earlyStart
+
     def setEarlyStart(self, earlyStart):
         self.earlyStart = earlyStart
+
     def getEarlyFinish(self):
         return self.earlyFinish
+
     def setEarlyFinish(self, earlyFinish):
         self.earlyFinish = earlyFinish
+
     def getLateStart(self):
         return self.lateStart
+
     def getLateFinish(self):
         return self.lateFinish
+
     def setLateStart(self, lateStart):
         self.lateStart = lateStart
+
     def setLateFinish(self, lateFinish):
         self.lateFinish = lateFinish
+
     def getDuration(self):
         return self.duration
+
     def setDuration(self, duration):
         self.duration = duration
+
     def getDescription(self):
         return self.description
+
     def setDescription(self, description):
         self.description = description
+
     def appendPredecessor(self, predecessor):
         if self.predecessors == None:
             self.predecessors = [predecessor]
-        else: 
+        else:
             self.predecessors.append(predecessor)
+
     def appendSuccessor(self, successor):
         if self.successors == None:
             self.successors = [successor]
         else:
             self.successors.append(successor)
-        
+
     def getAllPredaecessors(self):
         if self.predecessors == None:
             return []
         else:
             return list(set(self.predecessors + [x.getAllPredaecessors() for x in self.predecessors]))
+
     def getAllSuccessors(self):
         if self.successors == None:
             return []
         else:
             return list(set(self.successors + [x.getAllSuccessors() for x in self.successors]))
-    
+
     def getNamesofPredaecessors(self):
         if self.predecessors == None or self.predecessors == []:
             return []
         else:
             return [x.getName() for x in self.predecessors]
-    
+
     def getNamesofSuccessors(self):
         if self.successors == None or self.successors == []:
             return []
         else:
             return [x.getName() for x in self.successors]
-        
-    
--- Assignment 4/Pert.py
+++ Assignment 4/Pert.py
@@ -2,31 +2,38 @@ import numpy as np
 import pandas as pd
 from Node import node as no
 
+
 class pert:
-    def __init__(self, nodes = None):
+    def __init__(self, nodes=None):
         self.nodes = nodes
-        
+
     def getNodes(self):
-        return  self.nodes
-    
+        return self.nodes
+
     def setNodes(self, nodes):
         self.nodes = nodes
-    
+
     def changepredecessor(self, node, predecessor):
         node.setPredecessor(predecessor)
+
     def changesuccessor(self, node, successor):
         node.setSuccessor(successor)
+
     def changetime(self, node, time):
         node.setTime(time)
+
     def changemintime(self, node, starttime):
         node.setMinTime(starttime)
+
     def changemaxtime(self, node, endtime):
         node.setEndTime(endtime)
+
     def changeexpectedtime(self, node, expected):
         node.setExpected(expected)
+
     def changefinished(self, node, finished):
         node.setFinished(finished)
-    
+
     def calculateLateDates(self):
         nodes = self.getNodes()
         while len(nodes) > 0:
@@ -37,7 +44,7 @@ class pert:
                         successor.calculateLateStart()
                         nodes.remove(successor)
         print("Late dates calculated")
-    
+
     def calculateEarlyDates(self):
         nodes = self.getNodes()
         while len(nodes) > 0:
@@ -49,27 +56,30 @@ class pert:
                         nodes.remove(predecessor)
         print("Early dates calculated")
 
+
 class loader():
-    def __init__(self,filename):
+    def __init__(self, filename):
         self.filename = filename
         self.nodes = []
         self.load()
-    
+
     def returnNodes(self):
         return self.nodes
-    
+
     def addNode(self, node):
         self.nodes.append(node)
+
     def getNodeByName(self, name):
         for node in self.nodes:
             if node.getName() == name:
                 return node
+
     def load(self):
         df = pd.read_excel(self.filename)
         df = df.dropna(how='all')
-    #def __init__(self, name, time, duration = np.inf, predecessors = None, successors = None, finished = False, description = None):
+    # def __init__(self, name, time, duration = np.inf, predecessors = None, successors = None, finished = False, description = None):
         for index, rows in df.iterrows():
-            #print(rows[1])
+            # print(rows[1])
             name = rows["Codes"]
             try:
                 description = rows["Descriptions"]
@@ -79,20 +89,20 @@ class loader():
             predecessors = rows["Predecessors"]
             if name == "Start":
                 predecessors = None
-                time = [0,0,0]
-                task  = no(name, time, predecessors, None, False, description)
-            else: 
+                time = [0, 0, 0]
+                task = no(name, time, predecessors, None, False, description)
+            else:
                 predecessors = predecessors.split(", ")
                 if name == "Completion" or name == "End":
-                    time = [0,0,0]
+                    time = [0, 0, 0]
                 else:
-                    #print(rows)
+                    # print(rows)
                     time = str(time).strip("()")
                     time = time.split(", ")
                     for tall in time:
                         tall = int(tall)
                     # print(time)
-                task  = no(name, time, predecessors, None, False, description)
+                task = no(name, time, predecessors, None, False, description)
             self.addNode(task)
         for node in self.nodes:
             sucsessors = []
@@ -113,44 +123,48 @@ class loader():
             sucsessors = []
             if node.getName() != "Start":
                 for i in range(len(node.getPredecessor())):
-                    predecessors.append(self.getNodeByName(node.getPredecessor()[i]))
+                    predecessors.append(
+                        self.getNodeByName(node.getPredecessor()[i]))
             if node.getName() != "Completion":
                 for i in range(len(node.getSuccessor())):
-                    sucsessors.append(self.getNodeByName(node.getSuccessor()[i]))
+                    sucsessors.append(
+                        self.getNodeByName(node.getSuccessor()[i]))
             node.setPredecessor(predecessors)
             node.setSuccessor(sucsessors)
 
 
 class printer:
-    def __init__(self, Pert = None):
+    def __init__(self, Pert=None):
         if Pert != None:
             self.nodes = Pert.getNodes()
         else:
             self.nodes = None
         self.printproject()
-    
+
     def printproject(self):
         if self.nodes == None:
             return
         for node in self.nodes:
             self.printNode(node)
-    
+
     def printNode(self, Node):
-            predacessors = Node.getNamesofPredaecessors()
-            successors = Node.getNamesofSuccessors()
-            print(f"Name: {Node.getName()}, description: {Node.getDescription()}, durations: {Node.getTime()}, early dates: {Node.getEarlyStart(), Node.getEarlyFinish()}, late dates: {Node.getLateStart(), Node.getLateFinish()}, critical: {Node.isCritical()}, predecessors: {predacessors}, successors: {successors}")
+        predacessors = Node.getNamesofPredaecessors()
+        successors = Node.getNamesofSuccessors()
+        print(f"Name: {Node.getName()}, description: {Node.getDescription()}, durations: {Node.getTime()}, early dates: {Node.getEarlyStart(), Node.getEarlyFinish()}, late dates: {Node.getLateStart(), Node.getLateFinish()}, critical: {Node.isCritical()}, predecessors: {predacessors}, successors: {successors}")
+
 
 class calculator:
-    def __init__(self, project = None):
+    def __init__(self, project=None):
         self.project = project
         self.calculate()
+
     def calculate(self):
         startlist = self.project.getNodes()
         while len(startlist) > 0:
             for node in startlist:
                 for node2 in node.getPredecessor():
                     if node2 in startlist:
-                        continue   
+                        continue
                 self.calculateEarlyStart(node)
                 self.calculateEarlyFinish(node)
                 startlist.remove(node)
@@ -159,38 +173,40 @@ class calculator:
             self.calculateLateStart(node)
         for node in self.project.getNodes():
             self.checkIfCritical(node)
-        
-    
+
     def calculateEarlyStart(self, Node):
-        #print(Node.getPredecessor())
+        # print(Node.getPredecessor())
         printers = printer()
         printers.printNode(Node)
         if Node.getPredecessor() == []:
             Node.setEarlyStart(0)
         else:
             printers.printNode(Node.getPredecessor()[0])
-            sorted_list = sorted(Node.getPredecessor(), key=lambda obj: obj.getEarlyFinish(), reverse=True)
+            sorted_list = sorted(Node.getPredecessor(
+            ), key=lambda obj: obj.getEarlyFinish(), reverse=True)
             Node.setEarlyStart(sorted_list[0].getEarlyFinish())
-        
+
     def calculateEarlyFinish(self, Node):
         if Node.getPredecessor() == []:
             Node.getEarlyFinish = Node.getTime()[1]
         Node.setEarlyFinish(Node.getEarlyStart() + Node.getDuration())
-    
+
     def calculateLateFinish(self, Node):
         if Node.successors == []:
             Node.setLateFinish(Node.getEarlyFinish())
         else:
-            Node.setLateFinish(min([x.getLateStart() for x in Node.getSuccessors()]))
-    
+            Node.setLateFinish(min([x.getLateStart()
+                               for x in Node.getSuccessors()]))
+
     def calculateLateStart(self, Node):
         Node.setLateStart(Node.getLateFinish() - Node.getDuration())
-    
+
     def checkIfCritical(self, Node):
         if Node.getEarlyStart() == Node.getLateStart():
             Node.setCritical(True)
         else:
             Node.setCritical(False)
+
 
 loaderr = loader("Assignment 4\Warehouse.xlsx")
 nodes = loaderr.returnNodes()
@@ -201,6 +217,4 @@ printer(project)
 calculatorr = calculator(project)
 printer(project)
 print("Nodes loaded")
-#nodes = loader("Assignment 4\Villa.xlsx")
-
-
+# nodes = loader("Assignment 4\Villa.xlsx")
